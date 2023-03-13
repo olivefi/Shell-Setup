caac395d9f8e80c3c04d961470cca74f3a18e619
olivefi_rsl_vm <olivefi@ethz.ch>
2023-03-13 04:19:32 -0700

    Add example zshrc

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # Install zsh-autosuggestions and source plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed 's/plugins=(/plugins=(zsh-autosuggestions /' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
 
 # Install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed 's/robbyrussell/powerlevel10k,/powerlevel10k/' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
+
+cp .example_zshrc ${HOME}/.zshrc
+
